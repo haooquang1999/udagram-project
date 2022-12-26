c1ff93658456d6bf145ba3aa0ea9593168c38c7b
hao quang <haooquang@gmail.com>
2022-12-26 21:54:36 +0700

    Udacity-project

---
+++ udagram/set_env.sh
@@ -0,0 +1,13 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=12345678
+export POSTGRES_HOST=postgres.ceahrljdchqz.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::quangdvh-bucket-udacity
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://localhost:8100
+export AWS_ACCESS_KEY_ID=AKIAX4BJUUYWVO6CNNKZ
+export AWS_SECRET_ACCESS_KEY=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
