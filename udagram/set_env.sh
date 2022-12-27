35414982733c8a492e158006aa3f5260ef5cb524
hao quang <haooquang@gmail.com>
2022-12-27 22:02:19 +0700

    udacity project

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -2,12 +2,12 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=12345678
-export POSTGRES_HOST=postgres.ceahrljdchqz.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=postgres.cboyjrfgdwrb.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::quangdvh-bucket-udacity
+export AWS_BUCKET=arn:aws:s3:::mybucket202726481452
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
 export URL=http://localhost:8100
-export AWS_ACCESS_KEY_ID=AKIAX4BJUUYWVO6CNNKZ
-export AWS_SECRET_ACCESS_KEY=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
+export AWS_ACCESS_KEY_ID=AKIAS6M3QTYWLY35M7MN
+export AWS_SECRET_ACCESS_KEY=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
