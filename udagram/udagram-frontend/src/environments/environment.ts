58090d5f0c794d46c29524c2aff42b4be70baa3f
hao quang <haooquang@gmail.com>
2022-12-27 22:22:13 +0700

    udacity project

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://appudacity-env.eba-yeskmtjd.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagram-api-dev222.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
